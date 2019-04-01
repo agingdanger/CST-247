59deace5cb1803856616ef8b4638b3e572aa294a
Charles Henderson <chenderson33@my.gcu.edu>
2019-04-01 08:31:46 -0700

    Added milestone 2 files

---
+++ RegLog/RegLog/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegLog.Models;
+
+namespace RegLog.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Log()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(user userModel)
+        {
+            using (seesharpEntities db = new seesharpEntities())
+            {
+                var userDetails = db.users.Where(x => x.email == userModel.email && x.firstname == userModel.firstname && x.lastname == userModel.lastname).FirstOrDefault();
+                if(userDetails == null)
+                {
+                    return View("Login", userModel);
+                }
+                else
+                {
+                    Session["ID"] = userDetails.ID;
+                    return RedirectToAction("Index" , "Home");
+                }
+            }
+           // return View();
+        }
+    }
+}
