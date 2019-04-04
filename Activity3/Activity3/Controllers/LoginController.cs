ed87296b61fcad5797eb2caa502267ef247374db
Charles Henderson <chenderson33@my.gcu.edu>
2019-04-04 16:49:26 -0700

    Added activity 3

---
+++ Activity3/Activity3/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using Activity3.Models;
+using Activity3.Services.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Login");
+            //return @"<b>Just a test from Index</b>";
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("Login");
+
+            SecurityService sec = new SecurityService();
+
+            bool auth = sec.Authenticate(model);
+
+            if (auth)
+            {
+                return View("LoginPassed");
+            }
+            else
+            {
+                return View("LoginFailed");
+            }
+        }
+    }
+}
---
+++ Activity3/Activity3/Services/Data/SecurityDAO.cs
@@ -0,0 +1,36 @@
+﻿using Activity3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace Activity3.Services.Data
+{
+    public class SecurityDAO
+    {
+        string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;initial catalog=Test ;Integrated Security=True;";
+        public bool FindByUser(UserModel user)
+        {
+
+            // SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["connString"].ConnectionString);
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand sql = new SqlCommand("SELECT * FROM [Users] WHERE USERNAME = @User  AND PASSWORD = @Pass", conn);
+
+            conn.Open();
+
+            SqlDataReader read = sql.ExecuteReader();
+
+            if (read.HasRows)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
